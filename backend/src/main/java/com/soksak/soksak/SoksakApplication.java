041eb6a96f48194e1297f199c0eedf40cc869f5a
yoo <yoo9077@gmail.com>
2026-06-02 16:09:41 +0900

    first commit

---
+++ backend/src/main/java/com/soksak/soksak/SoksakApplication.java
@@ -0,0 +1,13 @@
+package com.soksak.soksak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoksakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoksakApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/soksak/soksak/SoksakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soksak.soksak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoksakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
